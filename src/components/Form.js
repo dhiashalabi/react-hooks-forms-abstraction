aaa570420c5b4ecb64b033f8b9746ce2012293fe
Dhia' Alhaq Shalabi <dhia.shalabi@gmail.com>
2023-03-18 11:04:01 +0300

    feat: completed assignment

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,22 +1,51 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
 
 function Form() {
-  const [firstName, setFirstName] = useState("John");
-  const [lastName, setLastName] = useState("Henry");
+  const [formData, setFormData] = useState({
+    firstName: "John",
+    lastName: "Henry",
+    admin: false,
+  });
 
-  function handleFirstNameChange(event) {
-    setFirstName(event.target.value);
-  }
+  function handleChange(event) {
+    const name = event.target.name;
+    let value = event.target.value;
 
-  function handleLastNameChange(event) {
-    setLastName(event.target.value);
+    if (event.target.type === "checkbox") {
+      value = event.target.checked;
+    }
+
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
   }
 
   return (
     <form>
-      <input type="text" onChange={handleFirstNameChange} value={firstName} />
-      <input type="text" onChange={handleLastNameChange} value={lastName} />
-      <button type="submit">Submit</button>
+      <input
+        type="text"
+        name="firstName"
+        onChange={handleChange}
+        value={formData.firstName}
+      />
+      <input
+        type="text"
+        name="lastName"
+        onChange={handleChange}
+        value={formData.lastName}
+      />
+      <input
+        type="checkbox"
+        name="admin"
+        onChange={handleChange}
+        checked={formData.admin}
+      />
+      <Link to={`/formdata?firstName=${formData.firstName}&lastName=${formData.lastName}&admin=${formData.admin}`}>
+        <button type="submit">Submit</button>
+      </Link>
     </form>
   );
 }
